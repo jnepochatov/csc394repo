998471ec78934cd5b718bb9b875aadace22db6e0
jryan52 <jorgonryan@gmail.com>
2021-05-29 16:15:25 -0500

    Corrected algos

--- Persistence/candidate_algo.py
+++ Persistence/candidate_algo.py
@@ -1,23 +1,26 @@
-import Persistence.candidate_persistence as cd, Persistence.company_persistence as cp, Persistence.job_persistence as jb
+import candidate_persistence as cd, job_persistence as jb
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 def find_matches(candidate_id):
+    #Connect to the database
     client = MongoClient("mongodb+srv://jryan52:password1234@team2.14wgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    #Create a cursor to scroll through all available jobs
     cursor = client.myFirstDatabase.Job.find({})
-
+    #Create candidate object using ObjectId
     candidate = cd.candidate(candidate_id)
     c_tech    = [x.lower() for x in candidate.get_tech()]
     c_biz     = [x.lower() for x in candidate.get_business()]
     c_att     = [x.lower() for x in candidate.get_attitude()]
-    
+    c_matches = candidate.get_matches()
+    #Create matches array
     matches = []
 
-    if len(candidate.get_matches()) > 0:
-        for match in candidate.get_matches():
+    if len(c_matches) > 0:
+        for match in c_matches:
                 var_set = (match[0], int(match[1]))
                 matches.append(var_set)
-
+    #Scroll through cursor and check if jobs align with candidates various skills
     for doc in cursor:
         job_id = doc["_id"]
         job       = jb.job(job_id)
@@ -38,18 +41,24 @@ def find_matches(candidate_id):
         for skill in j_att:
             if skill in c_att:
                 attitude_count += 1
-        
+        #Create total score
         total_score = tech_count + business_count + attitude_count
-        if total_score > 0:
+        
+        if total_score > 0 and (job_id, total_score) not in matches:
+            #If the candidate doesn't have 10 matches yet, add the job to the match list
             if len(matches) < 10:
                 matches.append((job_id, total_score))
-                matches.sort(key = lambda x: x[1], reverse=True)
+
+            #If the candidate already has a list of 10 matches
             elif len(matches) == 10:
                 #check minimum score of last candidate
                 min_score = matches[-1][1]
-                #if score is lower than current candidate, replace in matches and sort
+                #if score is higher than current last place job, replace in matches and sort
                 if total_score > min_score:
                     del matches[-1]
                     matches.append((job_id, total_score))
-                    matches = matches.sort(key = lambda x: x[1])
-            candidate.db.update_one({"_id" : ObjectId(candidate_id)}, { "$set" : {"bestMatch" : matches}})
+            matches = matches.sort(key = lambda x: x[1])
+    #Update the best matches list in the database
+    candidate.db.update_one({"_id" : ObjectId(candidate_id)}, { "$set" : {"bestMatch" : matches}})
+
+#find_matches("60ac7f921ab56f248fd12fe0")
--- Persistence/job_algo.py
+++ Persistence/job_algo.py
@@ -1,31 +1,32 @@
-import candidate_persistence as cd, company_persistence as cp, job_persistence as jb
+import candidate_persistence as cd, job_persistence as jb
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
-#Take out job_id and make script to call all jobs when a new candidate is created
 
 def find_matches(job_id):
     client = MongoClient("mongodb+srv://jryan52:password1234@team2.14wgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     cursor = client.myFirstDatabase.Candidate.find({})
 
-    job = jb.job(job_id)
-    j_tech = [x.lower() for x in job.get_tech()]
-    j_biz = [x.lower() for x in job.get_business()]
-    j_att = [x.lower() for x in job.get_attitude()]
-    j_matches = []
+    job       = jb.job(job_id)
+    j_tech    = [x.lower() for x in job.get_tech()]
+    j_biz     = [x.lower() for x in job.get_business()]
+    j_att     = [x.lower() for x in job.get_attitude()]
+    j_matches = job.get_matches()
+    
+    matches = []
 
-    if len(job.get_matches()) > 0:
-        for match in job.get_matches():
+    if len(j_matches) > 0:
+        for match in j_matches:
                 var_set = (match[0], int(match[1]))
-                j_matches.append(var_set)
+                matches.append(var_set)
+
 
     for doc in cursor:
         candidate_id = doc["_id"]
         candidate = cd.candidate(candidate_id)
-        c_tech = [x.lower() for x in candidate.get_tech()]
-        c_biz = [x.lower() for x in candidate.get_business()]
-        c_att = [x.lower() for x in candidate.get_attitude()]
-        c_matches = candidate.get_matches()
+        c_tech    = [x.lower() for x in candidate.get_tech()]
+        c_biz     = [x.lower() for x in candidate.get_business()]
+        c_att     = [x.lower() for x in candidate.get_attitude()]
+        c_name    = candidate.get_username()
 
         tech_count, business_count, attitude_count = 0, 0, 0
 
@@ -43,16 +44,23 @@ def find_matches(job_id):
         
         total_score = tech_count + business_count + attitude_count
 
-        if total_score > 0:
-            if len(j_matches) < 10:
-                j_matches.append((candidate.get_username(), total_score))
-                j_matches.sort(key = lambda x: x[1], reverse=True)
-            elif len(j_matches) == 10:
+        if total_score > 0 and (c_name, total_score) not in matches:
+            
+            if len(matches) < 10:
+                matches.append((c_name, total_score))
+                
+            elif len(matches) == 10:
                 #check minimum score of last candidate
-                min_score = j_matches[-1][1]
+                min_score = matches[-1][1]
                 #if score is lower than current candidate, replace in matches and sort
                 if total_score > min_score:
-                    del j_matches[-1]
-                    j_matches.append((candidate.get_username(), total_score))
-                    j_matches = j_matches.sort(key = lambda x: x[1])
-            job.db.update_one({"_id" : ObjectId(job_id)}, { "$set" : {"bestMatch" : j_matches}})
+                    del matches[-1]
+                    matches.append((c_name, total_score))
+
+            matches.sort(key = lambda x: x[1], reverse=True)
+
+    if matches != None: 
+        matches = list(matches)       
+        job.db.update_one({"_id" : ObjectId(job_id)}, { "$set" : {"bestMatch" : matches}})
+
+#find_matches("60a97ff7fc1c5254e872f38a")
