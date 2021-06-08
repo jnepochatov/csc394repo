4651d3147404d35f35ebeee50b10b3ec9b92fd9d
Miguel Blancas <Mblanca4@depaul.edu>
2021-06-08 14:49:43 -0500

    Updated test file

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from hashlib import sha256
+from Persistence.job import JobObject
 
 dbs = MongoClient("mongodb+srv://Mblanca4:Team2SpringQuarter@team2.14wgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").myFirstDatabase
 candidates = dbs.Candidate
@@ -245,7 +246,59 @@ def del_companies():
     companies.remove( {} )
 
 def add_jobs():
-    return
+    job = JobObject(1, "accountant", "accountant", "Looking for a skilled accountant willing to put in the time.",
+                    ["python", "MATLAB"], ["Quickbooks", "Accounting", "Finance", "mathematics"],
+                    ["committed", "professional", "responsible"], list()).create()
+    job = JobObject(2, "programmer", "programming", "need experienced programmers", ["C", "C#", "C++", "Assembly"],
+                    ["communication", "problem solving", "critical thinking"],
+                    ["committed", "dependable", "professional"], list()).create()
+    job = JobObject(3, "software developer", "software developer", "need beginner software developers",
+                    ["Java", "python", "PL/SQL"], ["communication", "problem solving", "critical thinking"],
+                    ["committed", "dependable", "professional"], list()).create()
+    job = JobObject(4, "research assistant", "assist lead researcher", "help with research", ["PL/SQL"],
+                    ["communication", "Organization", "research"], ["respectful", "helpfulness", "responsible"],
+                    list()).create()
+    job = JobObject(5, "IT", "information technology", "IT role", ["HTML", "CSS", "JavaScript", "Java"],
+                    ["information technology"], ["helpfulness", "adaptable"], list()).create()
+
+    job = JobObject(6, "digital media specialist", "social media manager", "Looking to grow our social media presence.",
+                    list(), ["marketing/sales", "digital media", "writing", "graphic design"],
+                    ["enthusiastic", "adaptable", "creative"], list()).create()
+    job = JobObject(7, "marketing associate", "marketing associate",
+                    "need marketing associate to help relay our company message.", list(),
+                    ["digital media", "marketing/sales"], ["enthusiastic", "adaptable"], list()).create()
+    job = JobObject(8, "grocery manager", "grocery manager", "need grocery manager to handle shift leading", list(),
+                    ["customer service", "food services"], ["dependable"], list()).create()
+    job = JobObject(9, "insurance consultant", "insurance consultant", "in need of insurance consultant",
+                    ["MATLAB", "R"], ["Finance", "Insurance"], ["professional"], list()).create()
+    job = JobObject(10, "restaurant management", "restaurant management", "looking for a restaurant manager", list(),
+                    ["customer service", "food services", "multitasking"], ["dependable", "responsible"],
+                    list()).create()
+
+    job = JobObject(11, "newsletter columnist", "newsletter columnist", "looking for new columnist", list(),
+                    ["multilingual", "writing", "research"], ["committed", "professional"], list()).create()
+    job = JobObject(12, "economist", "economist", "economic advisor", ["R"], ["Finance", "Accounting"],
+                    ["dependable", "responsible", "professional"], list()).create()
+    job = JobObject(13, "head of human resources", "head of human resources", list(), list(), list(), list(),
+                    list()).create()
+    job = JobObject(14, "cloud engineer", "cloud engineer", "not looking for meteorologists", ["C++", "Swift"],
+                    ["critical thinking", "cloud computing"], ["innovative"], list()).create()
+    job = JobObject(15, "research analyst", "researcher", "analyzing research", ["PL/SQL", "R"], ["research"],
+                    ["committed", "professional"], list()).create()
+
+    job = JobObject(16, "tetris downstacker", "demolition", "it's tetris yo", list(), ["critical thinking"],
+                    ["enthusiastic", "cheerful", "innovative", "adaptable"], list()).create()
+    job = JobObject(17, "puyo exterminator", "exterminator", "time for puyo puyo", list(), ["critical thinking"],
+                    ["cheerful", "helpful", "adaptable"], list()).create()
+    job = JobObject(18, "architect", "opposite of demolition", "what should the buildings look like though", list(),
+                    ["Organization", "engineering"], ["innovative", "creative"], list()).create()
+    job = JobObject(19, "engineer", "how", "enginering...", ["MATLAB"], ["engineering"],
+                    ["innovative", "adaptable", "creative"], list()).create()
+    job = JobObject(20, "graphic design", "graphic designer", "whoah did you make this!", ["CSS"], ["graphic design"],
+                    ["creative"], list()).create()
+
+def del_jobs():
+    jobs.remove( {} )
 
 def test():
     company = list(companies.find())
